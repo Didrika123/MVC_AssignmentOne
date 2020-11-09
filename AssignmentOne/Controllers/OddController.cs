20c099f2322992dbf98648b8c34830ede69073e0
didrika@protonmail.com <didrika@protonmail.com>
2020-11-09 06:43:47 -0800

    Add project files.

---
+++ AssignmentOne/Controllers/OddController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AssignmentOne.Controllers
+{
+    public class OddController : Controller
+    {
+
+        [Route("/FeverCheck")]
+        public IActionResult FeverCheck()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/FeverCheck")]
+        public IActionResult FeverCheck(float temperature, string degreetype)
+        {
+            if(degreetype == "fahrenheit")
+                temperature = (temperature - 32 ) * 5 / 9; //wHy iS thIs sO WiErD
+
+            ViewBag.Temperature = temperature;
+            
+            return View();
+        }
+    }
+}
